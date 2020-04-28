114b06114a64821d8f8eee337139bd694997cf31
hakuna <hakuna@dev.com>
2020-04-29 00:01:10 +0200

    Finished source selection

--- src/main/java/simplecharacterbuilder/characterbuilder/personaldata/SourcePanel.java
+++ src/main/java/simplecharacterbuilder/characterbuilder/personaldata/SourcePanel.java
@@ -1,18 +1,27 @@
 package simplecharacterbuilder.characterbuilder.personaldata;
 
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.io.File;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 
+import simplecharacterbuilder.characterbuilder.util.SearchableComboBox;
+import simplecharacterbuilder.common.resourceaccess.GameFileAccessor;
+import simplecharacterbuilder.common.resourceaccess.PropertyRepository;
 import simplecharacterbuilder.common.uicomponents.ContentPanel;
 
 @SuppressWarnings("serial")
 public class SourcePanel extends ContentPanel {
-	private static final String[] PRIMARY_SOURCE_TYPES = new String[] {"Anime", "Manga", "Game", "Light Novel", "Eroge", "Other"};
-	private static final String[] SECONDARY_SOURCE_TYPES = new String[] {"<None>", "Anime", "Manga", "Game", "Light Novel", "Eroge", "Other"};
-	
+	private static final String[] PRIMARY_SOURCE_TYPES = new String[] { "Anime", "Manga", "Game", "Light Novel",
+			"Eroge", "Other" };
+	private static final String[] SECONDARY_SOURCE_TYPES = new String[] { "<None>", "Anime", "Manga", "Game",
+			"Light Novel", "Eroge", "Other" };
+
 	private static final int COMPONENT_HEIGHT = 20;
 	private static final int YPOS_FIRST_PANEL = 10;
 	private static final int YPOS_NEXT_PANEL = 25;
@@ -21,8 +30,15 @@ public class SourcePanel extends ContentPanel {
 	private static final String INSTALLMENT = "Installment";
 	private static final String PRIMARY_SOURCE_TYPE = "1st Type";
 	private static final String SECONDARY_SOURCE_TYPE = "2nd Type";
+	
+	private static final String DIRECTORY_NAME_REGEX = "^[0-9a-zA-Z_\\-. \\+'!#;&%$()=,@~]+";
+
+	private static final File ACTORS_DIRECTORY = GameFileAccessor.getFileFromProperty(PropertyRepository.ACTORS_FOLDER);
 
 	private final Map<String, JComboBox<String>> comboBoxes = new HashMap<>();
+
+	private SearchableComboBox franchiseComboBox;
+	private SearchableComboBox installmentComboBox;
 
 	public SourcePanel(int x, int y, int width, int height) {
 		super(x, y, width, height);
@@ -35,32 +51,70 @@ public class SourcePanel extends ContentPanel {
 
 	private void createFranchiseOptions() {
 		int yPos = YPOS_FIRST_PANEL;
-		String tooltip = "Select or add the franchise the character is from (i.e. 'Fate'). Make sure to check if it already exists.";
+		String tooltip = "Select or add the franchise the character is from (i.e. 'Fate'). The entered value must be a valid filename.";
 		createFormattedLabel(FRANCHISE + ":", 0, yPos, 75, COMPONENT_HEIGHT);
-		String[] options = new String[] { "a", "b"};
-		createComboBox(FRANCHISE, options, 85, yPos, 108, tooltip).setEditable(true);
+		String[] options = ACTORS_DIRECTORY.list((f, n) -> f.isDirectory());
+		this.franchiseComboBox = (SearchableComboBox) createComboBox(FRANCHISE, options, 85, yPos, 108, tooltip, true);
+		this.franchiseComboBox.setRegex(DIRECTORY_NAME_REGEX);
+		this.franchiseComboBox.addSelectionListener(() -> {
+			String[] installmentOptions = determineInstallmentOptions();
+			installmentComboBox.setOptions(installmentOptions);
+			String currentInstallmentSelection = (String) installmentComboBox.getEditor().getItem();
+			if(Arrays.binarySearch(installmentOptions, currentInstallmentSelection) < 0) {
+				installmentComboBox.getEditor().setItem("");
+				installmentComboBox.hidePopup();
+			}
+		});
 	}
 
 	private void createInstallmentOptions() {
 		int yPos = YPOS_FIRST_PANEL + YPOS_NEXT_PANEL;
-		String tooltip = "Select or add the installment of the franchise the character is from (i.e. 'Zero' for Fate Zero).";
+		String tooltip = "<html>Select or add the installment of the franchise the character is from (i.e. 'Zero' for Fate Zero).<br/>Only add this if necessary. The entered value must be a valid filename.</html>";
 		createFormattedLabel(INSTALLMENT + ":", 0, yPos, 75, COMPONENT_HEIGHT);
-		String[] options = new String[] { "a", "b"};
-		createComboBox(INSTALLMENT, options, 85, yPos, 108, tooltip).setEditable(true);
+		this.installmentComboBox = (SearchableComboBox) createComboBox(INSTALLMENT, new String[] {}, 85, yPos, 108,
+				tooltip, true);
+		this.installmentComboBox.setRegex(DIRECTORY_NAME_REGEX);
+		this.installmentComboBox.getEditor().getEditorComponent().addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent evt) {
+				installmentComboBox.setOptions(determineInstallmentOptions());
+				installmentComboBox.showPopup();
+			}
+		});
+	}
+
+	private String[] determineInstallmentOptions() {
+		String selectedFranchiseName = (String) this.franchiseComboBox.getEditor().getItem();
+		if (selectedFranchiseName == null || selectedFranchiseName.equals("")) {
+			return new String[] {};
+		}
+		File selectedFranchise = new File(ACTORS_DIRECTORY, selectedFranchiseName);
+		if (!selectedFranchise.exists()) {
+			return new String[] {};
+		}
+		String[] firstTierSubdirectories = selectedFranchise.list((f, n) -> f.isDirectory());
+		if (firstTierSubdirectories.length >= 0) {
+			boolean containsInfoXml = new File(selectedFranchise, firstTierSubdirectories[0])
+					.list((f, n) -> n.equals("Info.xml")).length > 0;
+			if (!containsInfoXml) {
+				return firstTierSubdirectories;
+			}
+		}
+		return new String[] {};
 	}
 
 	private void createPrimaryTypeOptions() {
 		int yPos = YPOS_FIRST_PANEL + 2 * YPOS_NEXT_PANEL;
 		String tooltip = "Select the type of the source the character first appeared in (compare 2nd Type).";
 		createFormattedLabel(PRIMARY_SOURCE_TYPE + ":", 0, yPos, 75, COMPONENT_HEIGHT);
-		createComboBox(PRIMARY_SOURCE_TYPE, PRIMARY_SOURCE_TYPES, 85, yPos, 108, tooltip);
+		createComboBox(PRIMARY_SOURCE_TYPE, PRIMARY_SOURCE_TYPES, 85, yPos, 108, tooltip, false);
 	}
 
 	private void createSecondaryOptions() {
 		int yPos = YPOS_FIRST_PANEL + 3 * YPOS_NEXT_PANEL;
 		String tooltip = "<html>Select the type of the source you based your implementation on if it's not the same as the 1st Type.<br/>Example: If you based your implementation on an anime adaption of a manga, the 1st Type would<br/>be 'Manga' and the 2nd Type 'Anime'.</html>";
 		createFormattedLabel(SECONDARY_SOURCE_TYPE + ":", 0, yPos, 75, COMPONENT_HEIGHT);
-		createComboBox(SECONDARY_SOURCE_TYPE, SECONDARY_SOURCE_TYPES, 85, yPos, 108, tooltip);
+		createComboBox(SECONDARY_SOURCE_TYPE, SECONDARY_SOURCE_TYPES, 85, yPos, 108, tooltip, false);
 	}
 
 	private JLabel createFormattedLabel(String text, int xPos, int yPos, int width, int height) {
@@ -73,9 +127,11 @@ public class SourcePanel extends ContentPanel {
 	}
 
 	private JComboBox<String> createComboBox(String name, String[] options, int xPos, int yPos, int width,
-			String tooltip) {
-		JComboBox<String> comboBox = new JComboBox<>(options);
-		comboBox.setSelectedIndex(0);
+			String tooltip, boolean searchable) {
+		JComboBox<String> comboBox = searchable ? new SearchableComboBox(options) : new JComboBox<String>(options);
+		if (options.length > 0) {
+			comboBox.setSelectedIndex(0);
+		}
 		comboBox.setBounds(xPos, yPos, width, 20);
 		comboBox.setToolTipText(tooltip);
 		this.comboBoxes.put(name, comboBox);
---
+++ src/main/java/simplecharacterbuilder/characterbuilder/util/SearchableComboBox.java
@@ -0,0 +1,143 @@
+package simplecharacterbuilder.characterbuilder.util;
+
+import java.awt.Toolkit;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.SwingUtilities;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.JTextComponent;
+import javax.swing.text.PlainDocument;
+
+@SuppressWarnings("serial")
+public class SearchableComboBox extends JComboBox<String> {
+	private final SearchableDocument searchableDocument;
+	private final List<Runnable> selectionListeners = new ArrayList<>();
+
+	private String[] unfilteredOptions;
+	private String regex;
+	
+	private boolean upDownKeyPressed;
+	private boolean optionsRecentlyFiltered;
+	private String lastSelection;
+
+	public SearchableComboBox(String[] options) {
+		super(options);
+		this.unfilteredOptions = options;
+		this.setEditable(true);
+		JTextComponent editor = (JTextComponent) this.getEditor().getEditorComponent();
+		editor.setDocument(this.searchableDocument = new SearchableDocument());
+		editor.addKeyListener(new NavigationKeyListener());
+		editor.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent evt) {
+				SwingUtilities.invokeLater(() -> editor.selectAll());
+				setPopupVisible(true);
+			}
+		});
+	}
+	
+	public void setOptions(String[] options) {
+		this.unfilteredOptions = options;
+		resetOptions();
+	}
+	
+	public void addSelectionListener(Runnable listener) {
+		this.selectionListeners.add(listener);
+	}
+
+	private class SearchableDocument extends PlainDocument {
+		private boolean textManipulationDisabled = false;
+
+		@Override
+		public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+			if (textManipulationDisabled) {
+				return;
+			}
+			if (regex != null && !str.matches(regex)) {
+				Toolkit.getDefaultToolkit().beep();
+				return;
+			}
+			super.insertString(offs, str, a);
+			updateOptions();
+		}
+
+		@Override
+		public void remove(int offs, int len) throws BadLocationException {
+			if (textManipulationDisabled) {
+				return;
+			}
+			super.remove(offs, len);
+			updateOptions();
+		}
+
+		private void updateOptions() throws BadLocationException {
+			String content = getText(0, getLength());
+			String[] filteredOptions = (String[]) Arrays.asList(unfilteredOptions).stream()
+					.filter(o -> o.toLowerCase().startsWith(content.toLowerCase())).toArray(String[]::new);
+			textManipulationDisabled = true;
+			setModel(new DefaultComboBoxModel<>(filteredOptions));
+			setPopupVisible(filteredOptions.length > 0);
+			textManipulationDisabled = false;
+			optionsRecentlyFiltered = true;
+		}
+	}
+
+	@Override
+	public void setSelectedItem(Object anObject) {
+		lastSelection = (String) anObject;
+		if (upDownKeyPressed) {
+			upDownKeyPressed = false;
+			return;
+		}
+		super.setSelectedItem(anObject);
+		SwingUtilities.invokeLater(() -> {
+			editor.selectAll();
+			resetOptions();
+			selectionListeners.stream().forEach(l -> l.run());
+		});
+	}
+
+	private class NavigationKeyListener extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_DOWN) {
+				upDownKeyPressed = true;
+				optionsRecentlyFiltered = false;
+			} else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+				String selection;
+				if (optionsRecentlyFiltered) {
+					selection = getModel().getElementAt(0);
+					optionsRecentlyFiltered = false;
+				} else {
+					selection = lastSelection;
+				}
+				getEditor().setItem(selection);
+				SwingUtilities.invokeLater(() -> editor.selectAll());
+				resetOptions();
+				selectionListeners.stream().forEach(l -> l.run());
+			}
+		}
+	}
+
+	private void resetOptions() {
+		searchableDocument.textManipulationDisabled = true;
+		setModel(new DefaultComboBoxModel<>(unfilteredOptions));
+		searchableDocument.textManipulationDisabled = false;
+	}
+
+	/**
+	 * Sets a regex that any input is matched against.
+	 */
+	public void setRegex(String regex) {
+		this.regex = regex;
+	}
+}
