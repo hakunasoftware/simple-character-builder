1e7ea393d7a7b407d5356f8363009abf0f44f3c5
Hakuna <hakuna@dev.com>
2020-07-18 19:50:27 +0200

    Adding items to equipment list added

--- src/main/java/simplecharacterbuilder/characterbuilder/core/CharacterBuilderControlPanel.java
+++ src/main/java/simplecharacterbuilder/characterbuilder/core/CharacterBuilderControlPanel.java
@@ -38,16 +38,16 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 	public static final int Y_POS = MAINPANEL_HEIGHT - CONTROLPANEL_HEIGHT - GAP_WIDTH;
 
 	private static final CharacterBuilderControlPanel INSTANCE = new CharacterBuilderControlPanel(X_POS, Y_POS);
-	
+
 	private final Marshaller marshaller;
 
 	private String firstName = "";
 	private String middleName = "";
 	private String lastName = "";
 	private boolean isAsian;
-	
+
 	private final JButton saveButton;
-	
+
 	private Map<String, String> cachedInstallments;
 
 	private List<CharacterBuilderMainComponent> mainComponents;
@@ -67,7 +67,7 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 		this.saveButton.addActionListener(e -> save());
 
 		this.button1.setEnabled(false);
-		
+
 		this.marshaller = JAXBContextHolder.createMarshaller();
 	}
 
@@ -76,10 +76,10 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 	}
 
 	public void next() {
-		if(checkItemEditingEnabled(mainComponents.get(pageIndex))) {
+		if (checkItemEditingEnabled(mainComponents.get(pageIndex))) {
 			return;
 		}
-		
+
 		if (!this.button1.isEnabled()) {
 			this.button1.setEnabled(true);
 		}
@@ -90,17 +90,18 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 			this.button2.setVisible(false);
 			this.saveButton.setVisible(true);
 		}
-		
-		if(mainComponents.get(pageIndex) instanceof SocialMainComponent && ((SocialMainComponent) mainComponents.get(pageIndex)).isEmpty()) {
+
+		if (mainComponents.get(pageIndex) instanceof SocialMainComponent
+				&& ((SocialMainComponent) mainComponents.get(pageIndex)).isEmpty()) {
 			next();
 		}
 	}
 
 	public void previous() {
-		if(checkItemEditingEnabled(mainComponents.get(pageIndex))) {
+		if (checkItemEditingEnabled(mainComponents.get(pageIndex))) {
 			return;
 		}
-		
+
 		if (!this.button2.isVisible()) {
 			this.saveButton.setVisible(false);
 			this.button2.setVisible(true);
@@ -113,26 +114,27 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 			this.button1.setEnabled(false);
 			this.button1.setBorder(CharacterBuilderMainComponent.BORDER);
 		}
-		
-		if(mainComponents.get(pageIndex) instanceof SocialMainComponent && ((SocialMainComponent) mainComponents.get(pageIndex)).isEmpty()) {
+
+		if (mainComponents.get(pageIndex) instanceof SocialMainComponent
+				&& ((SocialMainComponent) mainComponents.get(pageIndex)).isEmpty()) {
 			previous();
 		}
 	}
 
 	private synchronized void save() {
-		if(checkItemEditingEnabled(mainComponents.get(pageIndex))) {
+		if (checkItemEditingEnabled(mainComponents.get(pageIndex))) {
 			return;
 		}
-		
+
 		Actor actor = new Actor();
 		this.mainComponents.stream().forEach(c -> c.setValues(actor));
 //		verifyActor(actor);
 		confirmIntent();
-		
+
 		try {
 			String xml = marshallActorToInfoXmlString(actor);
 			System.out.println("Generated Info.xml:\n" + xml);
-			
+
 //			File characterFolder = getCharacterFolder(actor.getSource().getFranchise(), getInstallmentDirName(actor.getSource().getInstallment()), fullName);
 //			if(!characterFolder.mkdir()) {
 //				JOptionPane.showMessageDialog(null, "Character folder could not be created - it may already exist.", "Error", JOptionPane.ERROR_MESSAGE);
@@ -142,25 +144,31 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 //			writeStringToFile(xml, new File(characterFolder, "Info.xml"));
 //			
 //			BodyImageFileHolder.copyImagesToTargetDirectory(characterFolder);
+
+//			addActorToActorsList(actor);
+			addEquipmentToEquipmentList(actor);
 			
-			addActorToActorsList(actor);
 			PostInfoXmlGenerationRunnableHolder.runAll();
 			PostInfoXmlGenerationRunnableHolder.clear();
-			
-			JOptionPane.showMessageDialog(null, "Your informations were successfully saved! Don't forget to commit the changes and please do a quick check for any errors!");
+
+			JOptionPane.showMessageDialog(null,
+					"Your informations were successfully saved! Don't forget to commit the changes and please do a quick check for any errors!");
 		} catch (Exception e) {
 			PostInfoXmlGenerationRunnableHolder.clear();
-			JOptionPane.showMessageDialog(null, "An error occured during saving, sorry. Please report this so that it can be fixed! You may need to clean up some changes that were already made.", "Error", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null,
+					"An error occured during saving, sorry. Please report this so that it can be fixed! You may need to clean up some changes that were already made.",
+					"Error", JOptionPane.ERROR_MESSAGE);
 			e.printStackTrace();
 		}
 	}
-	
+
 	private boolean checkItemEditingEnabled(CharacterBuilderMainComponent mainComponent) {
-		if(!(mainComponent instanceof EquipmentCreatorMainComponent)) {
+		if (!(mainComponent instanceof EquipmentCreatorMainComponent)) {
 			return false;
 		}
-		if(((EquipmentCreatorMainComponent) mainComponent).isEditingModeEnabled()) {
-			JOptionPane.showMessageDialog(null, "You need to finish editing all items before proceeding!", "Error", JOptionPane.ERROR_MESSAGE);
+		if (((EquipmentCreatorMainComponent) mainComponent).isEditingModeEnabled()) {
+			JOptionPane.showMessageDialog(null, "You need to finish editing all items before proceeding!", "Error",
+					JOptionPane.ERROR_MESSAGE);
 			return true;
 		}
 		return false;
@@ -169,9 +177,9 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 	private boolean verifyActor(Actor actor) {
 		List<String> verificationErrors = getVerificationErrors(actor);
 		boolean errorsFound = verificationErrors.size() > 0;
-		if(errorsFound) {
+		if (errorsFound) {
 			StringBuilder errorMsg = new StringBuilder("Values are missing or invalid: ");
-			for(String error : verificationErrors) {
+			for (String error : verificationErrors) {
 				errorMsg.append("\n- ").append(error);
 			}
 			JOptionPane.showMessageDialog(null, errorMsg.toString(), "Error", JOptionPane.ERROR_MESSAGE);
@@ -179,59 +187,85 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 		}
 		return errorsFound;
 	}
-	
+
 	private void confirmIntent() {
-		if(!statGenerator.confirmIntentIfWarningsExist()) {
+		if (!statGenerator.confirmIntentIfWarningsExist()) {
 			PostInfoXmlGenerationRunnableHolder.clear();
 			return;
 		}
-		int dialogResult = JOptionPane.showConfirmDialog (null, "Are you sure you want to write your input into the respective files?", "Confirm Saving", JOptionPane.YES_NO_OPTION);
-		if(dialogResult != JOptionPane.YES_OPTION){
+		int dialogResult = JOptionPane.showConfirmDialog(null,
+				"Are you sure you want to write your input into the respective files?", "Confirm Saving",
+				JOptionPane.YES_NO_OPTION);
+		if (dialogResult != JOptionPane.YES_OPTION) {
 			PostInfoXmlGenerationRunnableHolder.clear();
 			return;
 		}
 	}
-	
+
 	private String marshallActorToInfoXmlString(Actor actor) throws JAXBException {
 		StringWriter writer = new StringWriter();
 		this.marshaller.marshal(actor, writer);
 		Actor.Name name = actor.getName();
 		String fullName = ValueFormatter.formatFullName(name.getFirst(), name.getMiddle(), name.getLast(), false);
 		String xml = writer.toString().replace("<Actor>", "<Actor><!-- " + fullName + " -->");
-		xml = addEmptyLinesAfterTags(xml, "</Likes>", "</Jobs>", "</Quest>", "</Body>", "</Source>", "</Stats>", "</Skills>");
+		xml = addEmptyLinesAfterTags(xml, "</Likes>", "</Jobs>", "</Quest>", "</Body>", "</Source>", "</Stats>",
+				"</Skills>");
 		return xml;
 	}
-	
+
 	private void addActorToActorsList(Actor actor) throws IOException {
 		Name name = actor.getName();
 		String fullName = ValueFormatter.formatFullName(name.getFirst(), name.getMiddle(), name.getLast(), false);
 		Source source = actor.getSource();
-		String sourceString = ValueFormatter.isEmpty(source.getInstallment()) ? source.getFranchise() : source.getInstallment();
+		String sourceString = ValueFormatter.isEmpty(source.getInstallment()) ? source.getFranchise()
+				: source.getInstallment();
 		File file = GameFileAccessor.getFileFromProperty(PropertyRepository.ACTORS_LIST);
 		File tempFile = new File(file.getParentFile(), file.getName() + ".tmp");
-		
-		try(BufferedReader reader = new BufferedReader(new FileReader(file));
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(file));
 				PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(tempFile)))) {
 			boolean actorAdded = false;
 			String line;
 			while ((line = reader.readLine()) != null) {
 				writer.println(line);
-			    if (!actorAdded && line.contains(sourceString)) {
-			    	writer.println(ValueFormatter.formatListEntry(fullName));
-			    	actorAdded = true;
-			    }
+				if (!actorAdded && line.contains(sourceString)) {
+					writer.println(ValueFormatter.formatListEntry(fullName));
+					actorAdded = true;
+				}
 			}
-			if(!actorAdded) {
+			if (!actorAdded) {
 				writer.println();
 				writer.println("#---- " + sourceString + " ----");
 				writer.println(ValueFormatter.formatListEntry(fullName));
 			}
 		}
-		
+
 		file.delete();
 		tempFile.renameTo(file);
 	}
-	
+
+	private void addEquipmentToEquipmentList(Actor actor) throws IOException {
+		List<String> equipments = actor.getEquipment().getEquip();
+		if (equipments.isEmpty()) {
+			return;
+		}
+
+		File file = GameFileAccessor.getFileFromProperty(PropertyRepository.EQUIPMENT_LIST);
+		File tempFile = new File(file.getParentFile(), file.getName() + ".tmp");
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(file));
+				PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(tempFile)))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				writer.println(line);
+			}
+			equipments.stream().forEach(e -> writer.println(ValueFormatter.formatListEntry(e)));
+		}
+
+		file.delete();
+		tempFile.renameTo(file);
+	}
+
 	private List<String> getVerificationErrors(Actor actor) {
 		List<String> errors = new ArrayList<>();
 		verifyActor(actor, errors);
@@ -239,70 +273,75 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 		return errors;
 	}
 
-	private void verifyActor(Actor actor, List<String> errors){
-		if(actor.getName() == null || ValueFormatter.isEmpty(actor.getName().getFirst()) && ValueFormatter.isEmpty(actor.getName().getLast())) {
+	private void verifyActor(Actor actor, List<String> errors) {
+		if (actor.getName() == null || ValueFormatter.isEmpty(actor.getName().getFirst())
+				&& ValueFormatter.isEmpty(actor.getName().getLast())) {
 			errors.add("Enter at least a first or a last name.");
 		}
-		if(actor.getName() != null && !ValueFormatter.isEmpty(actor.getName().getMiddle()) 
-				&& (ValueFormatter.isEmpty(actor.getName().getFirst()) || ValueFormatter.isEmpty(actor.getName().getLast()))) {
+		if (actor.getName() != null && !ValueFormatter.isEmpty(actor.getName().getMiddle())
+				&& (ValueFormatter.isEmpty(actor.getName().getFirst())
+						|| ValueFormatter.isEmpty(actor.getName().getLast()))) {
 			errors.add("To use a middle name, a first and a last name need to be set.");
 		}
-		if(actor.getSource() == null || ValueFormatter.isEmpty(actor.getSource().getFranchise())) {
+		if (actor.getSource() == null || ValueFormatter.isEmpty(actor.getSource().getFranchise())) {
 			errors.add("Enter a franchise.");
 		}
-		if(!actor.getType().equals("Slave") && actor.getMaintenance() == null) {
+		if (!actor.getType().equals("Slave") && actor.getMaintenance() == null) {
 			errors.add("Enter a maintenance fee or change the character's type to Slave.");
 		}
 	}
-	
+
 	private void verifyImages(List<String> errors) {
 		verifyImage(errors, ImageFileHolder.getPortrait(), "Load a portrait.");
 		verifyImage(errors, ImageFileHolder.getBodySprite(ImageFileHolder.BODY), "Load a body sprite.");
 		verifyImage(errors, ImageFileHolder.getBodySprite(ImageFileHolder.HAIR), "Load a hair sprite.");
 	}
-	
+
 	private void verifyImage(List<String> errors, File image, String errorMessage) {
-		if(image == null || !image.exists()) {
+		if (image == null || !image.exists()) {
 			errors.add(errorMessage);
 		}
 	}
 
 	private String getInstallmentDirName(String selectedInstallment) {
-		if(ValueFormatter.isEmpty(selectedInstallment)) {
+		if (ValueFormatter.isEmpty(selectedInstallment)) {
 			return null;
 		}
 		String cachedDir = cachedInstallments.get(selectedInstallment);
-		if(cachedDir != null) {
+		if (cachedDir != null) {
 			return cachedDir;
 		}
-		if(selectedInstallment.matches(GameFileAccessor.DIRECTORY_NAME_REGEX)) {
+		if (selectedInstallment.matches(GameFileAccessor.DIRECTORY_NAME_REGEX)) {
 			return selectedInstallment;
 		}
 		return getInstallmentDirFromUserInput(selectedInstallment);
 	}
-	
+
 	private String getInstallmentDirFromUserInput(String selectedInstallment) {
-		String input = JOptionPane.showInputDialog("Please enter a valid directory name for the installment directory (i.e. remove special characters).", selectedInstallment);
+		String input = JOptionPane.showInputDialog(
+				"Please enter a valid directory name for the installment directory (i.e. remove special characters).",
+				selectedInstallment);
 		boolean isValid = !ValueFormatter.isEmpty(input) && input.matches(GameFileAccessor.DIRECTORY_NAME_REGEX);
 		return isValid ? input : getInstallmentDirFromUserInput(selectedInstallment);
 	}
 
 	private File getCharacterFolder(String franchiseDirName, String installmentDirName, String fullCharacterName) {
-		File parentDir = new File(GameFileAccessor.getFileFromProperty(PropertyRepository.ACTORS_FOLDER), franchiseDirName);
-		if(!parentDir.exists()) {
+		File parentDir = new File(GameFileAccessor.getFileFromProperty(PropertyRepository.ACTORS_FOLDER),
+				franchiseDirName);
+		if (!parentDir.exists()) {
 			parentDir.mkdir();
 		}
-		if(installmentDirName != null) {
+		if (installmentDirName != null) {
 			parentDir = new File(parentDir, installmentDirName);
-			if(!parentDir.exists()) {
+			if (!parentDir.exists()) {
 				parentDir.mkdir();
 			}
 		}
 		return new File(parentDir, fullCharacterName);
 	}
-	
+
 	private void writeStringToFile(String input, File outputFile) throws IOException {
-		try (FileWriter writer = new FileWriter(outputFile)){
+		try (FileWriter writer = new FileWriter(outputFile)) {
 			writer.write(input);
 		}
 	}
@@ -310,9 +349,9 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 	private void updateNameLabel() {
 		this.nameLabel.setText(ValueFormatter.formatFullName(firstName, middleName, lastName, isAsian));
 	}
-	
+
 	private String addEmptyLinesAfterTags(String xml, String... tags) {
-		for(String tag : tags) {
+		for (String tag : tags) {
 			xml = xml.replace(tag, tag + "\n");
 		}
 		return xml;
@@ -321,9 +360,10 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 	public void init(List<CharacterBuilderComponent> components) {
 		mainComponents = components.stream().filter(c -> c instanceof CharacterBuilderMainComponent)
 				.map(c -> (CharacterBuilderMainComponent) c).collect(Collectors.toList());
-		statGenerator = (StatGenerator) mainComponents.stream().filter(c -> c instanceof StatGenerator).findFirst().get(); 
-		
-		for(int i = 1; i < mainComponents.size(); i++) {
+		statGenerator = (StatGenerator) mainComponents.stream().filter(c -> c instanceof StatGenerator).findFirst()
+				.get();
+
+		for (int i = 1; i < mainComponents.size(); i++) {
 			mainComponents.get(i).disable();
 		}
 	}
@@ -332,7 +372,7 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 		INSTANCE.firstName = firstName;
 		INSTANCE.updateNameLabel();
 	}
-	
+
 	public static void setMiddleName(String middleName) {
 		INSTANCE.middleName = middleName;
 		INSTANCE.updateNameLabel();
@@ -351,9 +391,9 @@ public class CharacterBuilderControlPanel extends ControlPanel {
 	public Map<String, String> getCachedInstallments() {
 		return cachedInstallments;
 	}
-	
+
 	public void setCachedInstallments(Map<String, String> cachedInstallments) {
 		this.cachedInstallments = cachedInstallments;
 	}
-	
+
 }
